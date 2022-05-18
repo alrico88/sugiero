24817f872fe785dea9fd2d3396251cba285090a9
onion chunky <78101139+ChunkyProgrammer@users.noreply.github.com>
2022-05-18 08:33:54 -0400

    add Yahoo and DuckDuckGo as result providers

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-type SuggestResult = [
+type GoogleSuggestResult = [
   string,
   string[],
   [],
   string[],
   {
-    'google:clientdata': { 'bpc':boolean, 'tlw':boolean },
+    'google:clientdata': { 'bpc': boolean, 'tlw': boolean },
     'google:suggestrelevance': number[],
     'google:suggestsubtypes': number[][],
     'google:suggesttype': string[],
@@ -14,48 +14,158 @@ type SuggestResult = [
   },
 ];
 
+type DuckDuckGoSuggestResult = {
+  'phrase': string
+}[]
+
+type YahooSuggestResult = {
+  q: string,
+  l: {
+    gprid: string
+  },
+  r: {
+    'k': string,
+    'm': number
+  }[]
+}
 type SuggestionType = 'QUERY' | 'NAVIGATION';
+type SearchEngine = 'Google' | 'Yahoo' | 'DuckDuckGo' | 'random'
+/**
+ * Gets search suggestions for a partial search
+ *
+ * @export
+ * @param {string} partialSearch The term to search suggestions for
+ * @param {SearchEngine} searchEngine
+ * @return {Promise<Suggestion[]>} The suggested searches
+ */
+ export async function getSuggestions(partialSearch: string, searchEngine: SearchEngine = "Google"): Promise<Suggestion[]> {
+  if (searchEngine == 'random') {
+    const engines = ['Google','Yahoo','DuckDuckGo']
+    searchEngine = engines[Math.floor(Math.random()*engines.length)] as SearchEngine
+  }
+  switch (searchEngine) {
+    case 'DuckDuckGo':
+      return new DuckDuckGo().getSuggestions(partialSearch)
+    case 'Yahoo':
+      return new Yahoo().getSuggestions(partialSearch)
+    default:
+      return new Google().getSuggestions(partialSearch)
+  }
+};
 
 export interface Suggestion {
   term: string,
   type: SuggestionType
 }
 
-/**
- * Gets the URL to query the autosuggest service
- *
- * @param {string} searchTerm
- * @return {string}
- */
-function getUrl(searchTerm: string): string {
-  return `http://suggestqueries.google.com/complete/search?client=chrome&q=${searchTerm}`;
+interface BaseSearch {
+  getUrl(searchTerm: string): string
+  getSuggestions(partialSearch: string): Promise<Suggestion[]>
 }
 
-/**
- * Type enforce the suggestion type
- *
- * @param {string} typeStr
- * @return {SuggestionType}
- */
-function getSuggestionType(typeStr: string): SuggestionType {
-  return typeStr === 'QUERY' ? 'QUERY' : 'NAVIGATION';
+export class Google implements BaseSearch {
+  /**
+   * Type enforce the suggestion type
+   *
+   * @param {string} typeStr
+   * @return {SuggestionType}
+   */
+  getSuggestionType(typeStr: string): SuggestionType {
+    return typeStr === 'QUERY' ? 'QUERY' : 'NAVIGATION';
+  }
+
+  /**
+   * Gets the URL to query the autosuggest service
+   *
+   * @param {string} searchTerm
+   * @return {string}
+   */
+  getUrl(searchTerm: string): string {
+    return `http://suggestqueries.google.com/complete/search?client=chrome&q=${searchTerm}`;
+  }
+  /**
+   * Gets search suggestions for a partial search
+   *
+   * @param {string} partialSearch The term to search suggestions for
+   * @return {Promise<Suggestion[]>} The suggested searches
+   */
+  async getSuggestions(partialSearch: string): Promise<Suggestion[]> {
+    const url = this.getUrl(partialSearch);
+
+    const res = await axios(url);
+    const suggestions = await res.data as GoogleSuggestResult;
+
+    return suggestions[1].map((suggestion, index) => ({
+      term: suggestion,
+      type: this.getSuggestionType(suggestions[4]['google:suggesttype'][index]),
+    }));
+  }
 }
 
-/**
- * Gets search suggestions for a partial search
- *
- * @export
- * @param {string} partialSearch The term to search suggestions for
- * @return {Promise<Suggestion[]>} The suggested searches
- */
-export async function getSuggestions(partialSearch: string): Promise<Suggestion[]> {
-  const url = getUrl(partialSearch);
+export class DuckDuckGo implements BaseSearch {
+  /**
+   * Gets the URL to query the autosuggest service
+   *
+   * @param {string} searchTerm
+   * @return {string}
+   */
+  getUrl(searchTerm: string): string {
+    return `https://duckduckgo.com/ac/?q=${searchTerm}&kl=wt-wt`;
+  }
+  /**
+   * Gets search suggestions for a partial search
+   *
+   * @param {string} partialSearch The term to search suggestions for
+   * @return {Promise<Suggestion[]>} The suggested searches
+   */
+  async getSuggestions(partialSearch: string): Promise<Suggestion[]> {
+    const url = this.getUrl(partialSearch);
 
-  const res = await axios(url);
-  const suggestions = await res.data as SuggestResult;
+    const res = await axios(url);
+    const suggestions = await res.data as DuckDuckGoSuggestResult;
 
-  return suggestions[1].map((suggestion, index) => ({
-    term: suggestion,
-    type: getSuggestionType(suggestions[4]['google:suggesttype'][index]),
-  }));
+    return suggestions.map((suggestion) => ({
+      term: suggestion.phrase,
+      type: 'QUERY',
+    }));
+  }
+}
+
+export class Yahoo implements BaseSearch {
+
+  /**
+   * Type enforce the suggestion type
+   *
+   * @param {number} typeNum
+   * @return {SuggestionType}
+   */
+  getSuggestionType(typeNum: number): SuggestionType {
+    return typeNum === 6 ? 'QUERY' : 'NAVIGATION';
+  }
+  /**
+   * Gets the URL to query the autosuggest service
+   *
+   * @param {string} searchTerm
+   * @return {string}
+   */
+  getUrl(searchTerm: string): string {
+    return `https://search.yahoo.com/sugg/gossip/gossip-us-ura/?command=${searchTerm}&output=sd1&appid=yfp-t&nresults=10&pq=`;
+  }
+  /**
+   * Gets search suggestions for a partial search
+   *
+   * @param {string} partialSearch The term to search suggestions for
+   * @return {Promise<Suggestion[]>} The suggested searches
+   */
+  async getSuggestions(partialSearch: string): Promise<Suggestion[]> {
+    const url = this.getUrl(partialSearch);
+
+    const res = await axios(url);
+    const suggestions = await res.data as YahooSuggestResult;
+
+    return suggestions.r.map((suggestion) => ({
+      term: suggestion.k,
+      type: this.getSuggestionType(suggestion.m),
+    }));
+  }
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,14 +1,55 @@
-import { getSuggestions } from '../src';
-
-describe('Test getting suggestions', () => {
+import { getSuggestions } from '../src'
+describe('Test getting Google suggestions', () => {
   it('Should return suggestions for common search terms', async () => {
-    const suggestions = await getSuggestions('hotels');
+    const suggestions = await getSuggestions('hotels', 'Google');
 
     expect(suggestions.length).toBeGreaterThan(0);
   });
 
   it('Should should not return suggestions for weird search terms', async () => {
-    const suggestions = await getSuggestions('saduqlkjasdku,ylkajsdkhasd');
+    const suggestions = await getSuggestions('aduqlkjasdku,ylkajsdkhasd', 'Google');
+
+    expect(suggestions.length).toBe(0);
+  });
+});
+
+describe('Test getting DuckDuckGo suggestions', () => {
+  it('Should return suggestions for common search terms', async () => {
+    const suggestions = await getSuggestions('hotels', 'DuckDuckGo');
+
+    expect(suggestions.length).toBeGreaterThan(0);
+  });
+
+  it('Should should not return suggestions for weird search terms', async () => {
+    const suggestions = await getSuggestions('saduqlkjasdku,ylkajsdkhasd', 'DuckDuckGo');
+
+    expect(suggestions.length).toBe(0);
+  });
+});
+
+describe('Test getting Yahoo suggestions', () => {
+  it('Should return suggestions for common search terms', async () => {
+    const suggestions = await getSuggestions('hotels', 'Yahoo');
+
+    expect(suggestions.length).toBeGreaterThan(0);
+  });
+
+  it('Should should not return suggestions for weird search terms', async () => {
+    const suggestions = await getSuggestions('saduqlkjasdku,ylkajsdkhasd', 'Yahoo');
+
+    expect(suggestions.length).toBe(0);
+  });
+});
+
+describe('Test getting random suggestions', () => {
+  it('Should return suggestions for common search terms', async () => {
+    const suggestions = await getSuggestions('hotels', 'random');
+
+    expect(suggestions.length).toBeGreaterThan(0);
+  });
+
+  it('Should should not return suggestions for weird search terms', async () => {
+    const suggestions = await getSuggestions('saduqlkjasdku,ylkajsdkhasd', 'random');
 
     expect(suggestions.length).toBe(0);
   });
